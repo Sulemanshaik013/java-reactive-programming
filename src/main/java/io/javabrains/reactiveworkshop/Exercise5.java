4efb1b14c3f7628c3dd4e718ab2a289008937930
Sulemanshaik013 <Sulemanshaik013@gmail.com>
2024-03-10 23:38:59 +0530

    completed task 5

--- src/main/java/io/javabrains/reactiveworkshop/Exercise5.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise5.java
@@ -2,20 +2,54 @@ package io.javabrains.reactiveworkshop;
 
 import java.io.IOException;
 
+import org.reactivestreams.Subscription;
+
+import reactor.core.publisher.BaseSubscriber;
+
 public class Exercise5 {
 
-    public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
 
-        // Use ReactiveSources.intNumberMono() and ReactiveSources.userMono()
+		// Use ReactiveSources.intNumberMono() and ReactiveSources.userMono()
 
-        // Subscribe to a flux using the error and completion hooks
-        // TODO: Write code here
+		// Subscribe to a flux using the error and completion hooks
+		ReactiveSources.intNumbersFlux().subscribe(x -> System.out.println(x),
+				error -> System.out.println(error.getLocalizedMessage()), () -> System.out.println("Completed"));
 
-        // Subscribe to a flux using an implementation of BaseSubscriber
-        // TODO: Write code here
+		// Subscribe to a flux using an implementation of BaseSubscriber
+		ReactiveSources.intNumbersFlux().subscribe(new MySubscriber<>());
+		
+		System.out.println("Press a key to end");
+		System.in.read();
+	}
 
-        System.out.println("Press a key to end");
-        System.in.read();
-    }
+}
+
+class MySubscriber<T> extends BaseSubscriber<T> {
+
+	@Override
+	protected void hookOnSubscribe(Subscription subscription) {
+		System.out.println("Subscribed!!");
+		request(1);
+	}
+
+	@Override
+	protected void hookOnComplete() {
+		System.out.println("Completed!!");
+	}
+
+	@Override
+	protected void hookOnError(Throwable throwable) {
+		System.out.println("error: "+throwable.getLocalizedMessage());
+	}
+
+	@Override
+	protected void hookOnNext(T value) {
+		System.out.println("value: "+value);
+		
+		request(1);
+	}
+	
+	
 
 }
